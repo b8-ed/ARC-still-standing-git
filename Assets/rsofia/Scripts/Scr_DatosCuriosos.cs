cfe522ee75bbdd1eb655e3f0b4fed532fd601c16
Sofia Brown <ruthsofiab@gmail.com>
2017-09-25 08:44:14 -0500

    Carga de escena con datos curiosos

---
+++ Assets/rsofia/Scripts/Scr_DatosCuriosos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Scr_DatosCuriosos : MonoBehaviour {
+
+    [Tooltip("Datos curiosos: se hara random entre este arreglo. Formato Text Mesh Pro")]
+    public string[] datosCuriosos;
+    public TextMeshProUGUI txtDisplay;
+    [Header("Carga de escena")]
+    public Scr_SceneManager sceneManager;
+    public float segundosEnPantalla = 5;
+    public string escenaACargar = "GameScene";
+
+    int randomIndex = 0;
+
+    private void Start()
+    {
+        txtDisplay.text = GetRandomText();
+        sceneManager.LoadSceneAfterSeconds(escenaACargar, segundosEnPantalla);
+    }
+
+    string GetRandomText()
+    {
+        randomIndex = Random.Range(0, datosCuriosos.Length);
+        return datosCuriosos[randomIndex];
+    }
+
+    
+}
---
+++ Assets/rsofia/Scripts/Scr_SceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scr_SceneManager : MonoBehaviour {
+
+	public void LoadSceneAfterSeconds(string _name, float _s)
+    {
+        StartCoroutine(WaitToLoadScene(_name, _s));
+    }
+
+    IEnumerator WaitToLoadScene(string _name, float _s)
+    {
+        yield return new WaitForSeconds(_s);
+        SceneManager.LoadScene(_name);
+    }
+
+}
