1cafea40521c5b148784451c78e1516e2fd8177c
Sofia Brown <ruthsofiab@gmail.com>
2017-10-06 08:59:01 -0500

    avances procedural

--- Assets/Test/rsofia/Scripts/Niveles Procedurales/N_Base.cs
+++ Assets/Test/rsofia/Scripts/Niveles Procedurales/N_Base.cs
@@ -11,8 +11,9 @@ public class N_Base : MonoBehaviour
     public Transform gridParent;
     public N_Modules[] piezasModularesPrefab;
     public N_Modules[] corners;
+    public N_Modules[] walls;
     Vector2 maxGridSize = new Vector2(10, 15); //en metros
-    int moduleMeasure = 3;
+    int moduleMeasure = 4;
 
     N_Grid[,] grid;
 
@@ -25,6 +26,7 @@ public class N_Base : MonoBehaviour
                 grid[i, j] = new N_Grid();
         }
         MakeLevel();
+        PrintGrid();
     }
 
     public void MakeLevel()
@@ -38,38 +40,41 @@ public class N_Base : MonoBehaviour
                 {
                     //upper left corner
                     if (i == 0)
-                        GetPrefab(i, j, true, true);
+                        GetPrefab(i, j, true, true, 90);
                     else if (i == maxGridSize.x - 1) //lower left corner
-                        GetPrefab(i, j, true, true);
+                        GetPrefab(i, j, true, true, 180);
                     else //wall left
-                        GetPrefab(i, j, false, true);
+                    {
+                        GetPrefab(i, j, false, true, 0);
+                        Debug.LogWarning("HELLO THERE " + i + " " + j);
+                    }
                 } //Up
                 else if (i == 0 && j > 0)
                 {
                     if (j == maxGridSize.y - 1) //upper right corner
-                        GetPrefab(i, j, true, true);
+                        GetPrefab(i, j, true, true, 0);
                     else //wall up
-                        GetPrefab(i, j, false, true);
+                        GetPrefab(i, j, false, true, 0);
                 }
                 //Right
                 else if(j == maxGridSize.y - 1)
                 {
                     if (i == 0 || i == maxGridSize.x - 1)
-                        GetPrefab(i, j, true, true);
+                        GetPrefab(i, j, true, true, 0);
                     else
-                        GetPrefab(i, j, false, true);
+                        GetPrefab(i, j, false, true, 0);
                 }//Wall Down
                 else if(i == maxGridSize.x - 1)
                 {
                     if(j == 0 || j == maxGridSize.y - 1)
-                        GetPrefab(i, j, true, true);
+                        GetPrefab(i, j, true, true, 180);
                     else
-                        GetPrefab(i, j, false, true);
+                        GetPrefab(i, j, false, true, 0);
 
                 } //Center stuff
                 else
                 {
-                    GetPrefab(i, j, false, false);
+                    GetPrefab(i, j, false, false, 0);
                 }
             }
         }
@@ -89,7 +94,7 @@ public class N_Base : MonoBehaviour
         }
     }
 
-    public void GetPrefab(int i, int j, bool _corner, bool _wall)
+    public void GetPrefab(int i, int j, bool _corner, bool _wall, float _rotation)
     {
         if(grid[i, j].idModule == 0)
         {
@@ -101,20 +106,18 @@ public class N_Base : MonoBehaviour
                 print("index" + index);
                 // grid[i, j].idModule = id;
                 grid[i, j].obj = Instantiate(corners[index].gameObject, gridParent);
+                if(id == 12)
+                    grid[i, j].obj.transform.Rotate(new Vector3(0, 0, -_rotation));
+                else
+                    grid[i, j].obj.transform.Rotate(new Vector3(0, 0, _rotation));
+                grid[i, j].idModule = id;
             }
             else if (_wall)
             {
-                for (int k = 0; k < corners.Length; k++)
-                {
-                    if (piezasModularesPrefab[k].isWall)
-                    {
-                        id = piezasModularesPrefab[k].id;
-
-                        grid[i, j].idModule = id;
-                        grid[i, j].obj = Instantiate(piezasModularesPrefab[k].gameObject, gridParent);
-                        break;
-                    }
-                }
+                int index = Random.Range(0, walls.Length);
+                id = walls[index].id;
+                grid[i, j].idModule = id;
+                grid[i, j].obj = Instantiate(walls[index].gameObject, gridParent);           
             }
             else
             {
@@ -128,7 +131,11 @@ public class N_Base : MonoBehaviour
                 {
                     for(int c = 0; c < piezasModularesPrefab[index].gridLayout[r].rowdata.Length; c++)
                     {
-                        //piezasModularesPrefab[index].gridLayout[i].rowdata[j];
+                        if(piezasModularesPrefab[index].gridLayout[r].rowdata[c] > 0)
+                        {
+                            grid[i, j].idModule = id;
+                        }
+                       
                     }
                 }
                 
@@ -137,5 +144,19 @@ public class N_Base : MonoBehaviour
         
 
         //return id;
+    }
+
+    void PrintGrid()
+    {
+        string line = "";
+        for(int i = 0; i < (int)maxGridSize.x; i++)
+        {
+            for(int j = 0; j < (int)maxGridSize.y; j++)
+            {
+                line += grid[i, j].idModule + " ";
+            }
+            print(line);
+            line = "";
+        }
     }
 }
