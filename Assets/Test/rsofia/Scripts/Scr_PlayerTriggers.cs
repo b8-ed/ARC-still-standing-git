eabe5e6403a15f9b14fa440d6564c6958adb9ccf
Sofia Brown <ruthsofiab@gmail.com>
2018-01-08 09:04:20 -0600

    Comienzo rotacion puertas

---
+++ Assets/Test/rsofia/Scripts/Scr_PlayerTriggers.cs
@@ -0,0 +1,23 @@
+﻿//ruth sofia brown
+//git rsofia
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scr_PlayerTriggers : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "DoorFrame")
+        {
+            OpenDoor(other.gameObject);
+        }
+    }
+
+    void OpenDoor(GameObject doorParent)
+    {
+        Transform door = doorParent.transform.parent;
+    }
+}
